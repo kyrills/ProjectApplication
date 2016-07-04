4c1a936bc71ce7078082a384118f499c8d02e987
Samuel <aardappel1234567890@gmail.com>
2016-07-04 10:48:33 +0200

    Nog meer getters

--- app/src/main/java/com/gerbendenboer/toptabtest/Data/MonthPNeighbourhood.java
+++ app/src/main/java/com/gerbendenboer/toptabtest/Data/MonthPNeighbourhood.java
@@ -42,7 +42,47 @@ public class MonthPNeighbourhood {
     public float HillegersbergAugustus = 0;
     public float HillegersbergSeptember = 0;
     public float OverschieJuni = 0;
-    public float OverschieJuli = 0; public float OverschieOverig = 0; public float OverschieApril = 0; public float OverschieAugustus = 0;public float OverschieSeptember = 0;public float kCrooswijkJuni = 0; public float kCrooswijkJuli = 0; public float kCrooswijkOverig = 0; public float kCrooswijkApril = 0; public float kCrooswijkAugustus = 0;public float kCrooswijkSeptember = 0;public float PernisJuni = 0; public float PernisJuli = 0; public float PernisOverig = 0; public float PernisApril = 0; public float PernisAugustus = 0;public float PernisSeptember = 0;public float IJsselmondeJuni = 0; public float IJsselmondeJuli = 0; public float IJsselmondeOverig = 0; public float IJsselmondeApril = 0; public float IJsselmondeAugustus = 0;public float IJsselmondeSeptember = 0;public float WestJuni = 0; public float WestJuli = 0; public float WestOverig = 0; public float WestApril = 0; public float WestAugustus = 0;public float WestSeptember = 0;public float OmoordJuni = 0; public float OmoordJuli = 0; public float OmoordOverig = 0; public float OmoordApril = 0; public float OmoordAugustus = 0;public float OmoordSeptember = 0;public float HoogvlietJuni = 0; public float HoogvlietJuli = 0; public float HoogvlietOverig = 0; public float HoogvlietApril = 0; public float HoogvlietAugustus = 0;public float HoogvlietSeptember = 0;
+    public float OverschieJuli = 0;
+    public float OverschieOverig = 0;
+    public float OverschieApril = 0;
+    public float OverschieAugustus = 0;
+    public float OverschieSeptember = 0;
+    public float kCrooswijkJuni = 0;
+    public float kCrooswijkJuli = 0;
+    public float kCrooswijkOverig = 0;
+    public float kCrooswijkApril = 0;
+    public float kCrooswijkAugustus = 0;
+    public float kCrooswijkSeptember = 0;
+    public float PernisJuni = 0;
+    public float PernisJuli = 0;
+    public float PernisOverig = 0;
+    public float PernisApril = 0;
+    public float PernisAugustus = 0;
+    public float PernisSeptember = 0;
+    public float IJsselmondeJuni = 0;
+    public float IJsselmondeJuli = 0;
+    public float IJsselmondeOverig = 0;
+    public float IJsselmondeApril = 0;
+    public float IJsselmondeAugustus = 0;
+    public float IJsselmondeSeptember = 0;
+    public float WestJuni = 0;
+    public float WestJuli = 0;
+    public float WestOverig = 0;
+    public float WestApril = 0;
+    public float WestAugustus = 0;
+    public float WestSeptember = 0;
+    public float OmoordJuni = 0;
+    public float OmoordJuli = 0;
+    public float OmoordOverig = 0;
+    public float OmoordApril = 0;
+    public float OmoordAugustus = 0;
+    public float OmoordSeptember = 0;
+    public float HoogvlietJuni = 0;
+    public float HoogvlietJuli = 0;
+    public float HoogvlietOverig = 0;
+    public float HoogvlietApril = 0;
+    public float HoogvlietAugustus = 0;
+    public float HoogvlietSeptember = 0;
 
     public float getCentrumJuni() {return CentrumJuni;}
     public float getCentrumJuli() {return CentrumJuli;}
@@ -53,8 +93,73 @@ public class MonthPNeighbourhood {
     public float getCharloisJuni() { return CharloisJuni;}
     public float getCharloisJuli() {return CharloisJuli;}
     public float getCharloisOverig() {return CharloisOverig;}
-//    public float getOktober() {return Oktober;}
-//    public float getNovember() {return November;}
-//    public float getDecember() { return December;}
-
+    public float getCharloisApril() { return CharloisApril;}
+    public float getCharloisAugustus() {return CharloisAugustus;}
+    public float getCharloisSeptember() {return CharloisSeptember;}
+    public float getDelfshavenJuni() { return DelfshavenJuni;}
+    public float getDelfshavenJuli() {return DelfshavenJuli;}
+    public float getDelfshavenOverig() {return DelfshavenOverig;}
+    public float getDelfshavenApril() { return DelfshavenApril;}
+    public float getDelfshavenAugustus() {return DelfshavenAugustus;}
+    public float getDelfshavenSeptember() {return DelfshavenSeptember;}
+    public float getFeijenoordJuni() { return FeijenoordJuni;}
+    public float getFeijenoordJuli() {return FeijenoordJuli;}
+    public float getFeijenoordOverig() {return FeijenoordOverig;}
+    public float getFeijenoordApril() { return FeijenoordApril;}
+    public float getFeijenoordAugustus() {return FeijenoordAugustus;}
+    public float getFeijenoordSeptember() {return FeijenoordSeptember;}
+    public float getNoordJuni() { return NoordJuni;}
+    public float getNoordJuli() {return NoordJuli;}
+    public float getNoordOverig() {return NoordOverig;}
+    public float getNoordApril() { return NoordApril;}
+    public float getNoordAugustus() {return NoordAugustus;}
+    public float getNoordSeptember() {return NoordSeptember;}
+    public float getHillegersbergJuni() { return HillegersbergJuni;}
+    public float getHillegersbergJuli() {return HillegersbergJuli;}
+    public float getHillegersbergOverig() {return HillegersbergOverig;}
+    public float getHillegersbergApril() { return HillegersbergApril;}
+    public float getHillegersbergAugustus() {return HillegersbergAugustus;}
+    public float getHillegersbergSeptember() {return HillegersbergSeptember;}
+    public float getOverschieJuni() { return OverschieJuni;}
+    public float getOverschieJuli() {return OverschieJuli;}
+    public float getOverschieOverig() {return OverschieOverig;}
+    public float getOverschieApril() { return OverschieApril;}
+    public float getOverschieAugustus() {return OverschieAugustus;}
+    public float getOverschieSeptember() {return OverschieSeptember;}
+    public float getkCrooswijkJuni() { return kCrooswijkJuni;}
+    public float getkCrooswijkJuli() {return kCrooswijkJuli;}
+    public float getkCrooswijkOverig() {return kCrooswijkOverig;}
+    public float getkCrooswijkApril() { return kCrooswijkApril;}
+    public float getkCrooswijkAugustus() {return kCrooswijkAugustus;}
+    public float getkCrooswijkSeptember() {return kCrooswijkSeptember;}
+    public float getPernisJuni() { return PernisJuni;}
+    public float getPernisJuli() {return PernisJuli;}
+    public float getPernisOverig() {return PernisOverig;}
+    public float getPernisApril() { return PernisApril;}
+    public float getPernisAugustus() {return PernisAugustus;}
+    public float getPernisSeptember() {return PernisSeptember;}
+    public float getIJsselmondeJuni() { return IJsselmondeJuni;}
+    public float getIJsselmondeJuli() {return IJsselmondeJuli;}
+    public float getIJsselmondeOverig() {return IJsselmondeOverig;}
+    public float getIJsselmondeApril() { return IJsselmondeApril;}
+    public float getIJsselmondeAugustus() {return IJsselmondeAugustus;}
+    public float getIJsselmondeSeptember() {return IJsselmondeSeptember;}
+    public float getWestJuni() { return WestJuni;}
+    public float getWestJuli() {return WestJuli;}
+    public float getWestOverig() {return WestOverig;}
+    public float getWestApril() { return WestApril;}
+    public float getWestAugustus() {return WestAugustus;}
+    public float getWestSeptember() {return WestSeptember;}
+    public float getOmoordJuni() { return OmoordJuni;}
+    public float getOmoordJuli() {return OmoordJuli;}
+    public float getOmoordOverig() {return OmoordOverig;}
+    public float getOmoordApril() { return OmoordApril;}
+    public float getOmoordAugustus() {return OmoordAugustus;}
+    public float getOmoordSeptember() {return OmoordSeptember;}
+    public float getHoogvlietJuni() { return HoogvlietJuni;}
+    public float getHoogvlietJuli() {return HoogvlietJuli;}
+    public float getHoogvlietOverig() {return HoogvlietOverig;}
+    public float getHoogvlietApril() { return HoogvlietApril;}
+    public float getHoogvlietAugustus() {return HoogvlietAugustus;}
+    public float getHoogvlietSeptember() {return HoogvlietSeptember;}
 }
